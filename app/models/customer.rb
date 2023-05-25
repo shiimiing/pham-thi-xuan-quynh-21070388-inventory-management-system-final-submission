fd56ff02d9af9f25e5d5c9cdd7a603a46efc4733
shiimiing <95361249+shiimiing@users.noreply.github.com>
2023-05-26 02:35:04 +0700

    Polishing plate boiler content

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ApplicationRecord
   belongs_to :staff
   has_many :orders
-  validates_presence_of :full_name, :address, :email, :username, :password
+  validates_presence_of :first_name, :address, :email
   validates_numericality_of :phone
-  validates_uniqueness_of :phone, :email, :username
+  validates_uniqueness_of :phone, :email
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,5 @@
 class Role < ApplicationRecord
   has_many :staffs
-  validates_presence_of :role_name
-  validates_uniqueness_of :role_name
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
