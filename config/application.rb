da26b5ee497d356595f431b101ec901de70489e0
shiimiing <95361249+shiimiing@users.noreply.github.com>
2023-05-26 11:24:47 +0700

    Config for blocked hosting

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module PhamThiXuanQuynh21070388InventoryManagementSystemFinalSubmission
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << "demo-rails-app-production.up.railway.app" 
+    config.hosts << "inventory-management-system-production-ac1f.up.railway.app"
   end
 end
